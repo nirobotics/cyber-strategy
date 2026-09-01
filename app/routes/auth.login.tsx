d133f323aa7eeaf4878deeb717515c8ad1ed88f8
dbdang_yi <2970099611@qq.com>
2026-09-02 01:09:32 +0800

    chore: clarify feature template ownership

--- app/routes/auth.login.tsx
+++ app/routes/auth.login.tsx
@@ -61,7 +61,7 @@ export default function LoginPage() {
           {demoLoading ? "Demo 加载中…" : "进入 Demo"}
         </Link>
       </main>
-      <AppFooter version="2026.1.65" />
+      <AppFooter version="2026.1.66" />
     </>
   );
 }
